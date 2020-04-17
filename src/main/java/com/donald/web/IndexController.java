03a012f36a35e2bc1a05b3a7275747c5d3702929
realdonald1994 <realdonald1994@outlook.com>
2020-04-17 19:59:52 -0400

    connect frontend to backend

---
+++ src/main/java/com/donald/web/IndexController.java
@@ -0,0 +1,25 @@
+package com.donald.web;
+
+import com.donald.NotFoundException;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.websocket.server.PathParam;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+//        int i =1/0;
+//        String blog =null;
+//        if(StringUtils.isBlank(blog)){
+//            throw  new NotFoundException("blog is not exist");
+//        }
+//        System.out.println("-------index-----");
+//        System.out.println(id+":"+name);
+        return "index";
+    }
+}
